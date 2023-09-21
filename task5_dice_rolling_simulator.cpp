c45221db89009eaecd043bc847c98622772e7dbc
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:54:25 +0300

    Create task5_dice_rolling_simulator.cpp

---
+++ task5_dice_rolling_simulator.cpp
@@ -0,0 +1,11 @@
+/*
+Task 5: Console Dice Rolling Simulator
+
+Description:
+   Implement a simulator for rolling a die with random number output.
+
+*/
+
+// Your code for Task 5 goes here...
+
+//=========================================================
