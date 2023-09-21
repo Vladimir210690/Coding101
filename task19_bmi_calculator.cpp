0b3e84b23fe9878f1ebb8f166d83359d4916f08e
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 23:02:20 +0300

    Create task19_bmi_calculator.cpp

---
+++ task19_bmi_calculator.cpp
@@ -0,0 +1,11 @@
+/*
+Task 19: Body Mass Calculator (BMI)
+
+Description:
+   Build an application for calculating Body Mass Index (BMI) based on user height and weight.
+
+*/
+
+// Your code for Task 19 goes here...
+
+//=========================================================
