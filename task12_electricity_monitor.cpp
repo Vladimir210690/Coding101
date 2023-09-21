b43c6738bce6c1dc8942db300e308f3ed77741e3
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:58:29 +0300

    Create task12_electricity_monitor.cpp

---
+++ task12_electricity_monitor.cpp
@@ -0,0 +1,11 @@
+/*
+Task 12: Electricity Consumption Monitor
+
+Description:
+   Develop an application for monitoring electricity consumption and energy conservation analysis.
+
+*/
+
+// Your code for Task 12 goes here...
+
+//=========================================================
