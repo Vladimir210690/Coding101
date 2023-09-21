655c249b55826669080eb1f4a3ff471a6eb5aa0b
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 23:01:54 +0300

    Create task18_password_manager.cpp

---
+++ task18_password_manager.cpp
@@ -0,0 +1,11 @@
+/*
+Task 18: Password Manager
+
+Description:
+   Create an application for storing and managing account passwords.
+
+*/
+
+// Your code for Task 18 goes here...
+
+//=========================================================
