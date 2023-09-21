a71067c673e037a66ced8f47c15db248f8aee186
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:53:47 +0300

    Create task4_interactive_quiz.cpp

---
+++ task4_interactive_quiz.cpp
@@ -0,0 +1,11 @@
+/*
+Task 4: Interactive Quiz
+
+Description:
+   Create a quiz with questions on various topics and multiple-choice answers.
+
+*/
+
+// Your code for Task 4 goes here...
+
+//=========================================================
