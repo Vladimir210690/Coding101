cf93e5902bcfcb63ea514e521248e884d41c035e
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 23:00:04 +0300

    Create task15_calorie_counter.cpp

---
+++ task15_calorie_counter.cpp
@@ -0,0 +1,11 @@
+/*
+Task 15: Calorie Counter
+
+Description:
+   Create an application to track food consumption and calculate total calorie intake throughout the day.
+
+*/
+
+// Your code for Task 15 goes here...
+
+//=========================================================
