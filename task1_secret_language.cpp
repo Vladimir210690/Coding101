74ae2466ada91eb178bcd9de3639c8a637cf38f1
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:50:03 +0300

    Create task1_secret_language.cpp

---
+++ task1_secret_language.cpp
@@ -0,0 +1,12 @@
+/*
+Task 1: Secret Language
+
+Description:
+   Develop a program for encrypting and decrypting texts
+   using a simple secret alphabet or letter substitution method.
+
+*/
+
+// Your code for Task 1 goes here...
+
+//=========================================================
