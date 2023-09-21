92f791d0cfee6e0a89f38805cc7041e04c4e6809
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:58:57 +0300

    Create task13_countdown_timer.cpp

---
+++ task13_countdown_timer.cpp
@@ -0,0 +1,11 @@
+/*
+Task 13: Countdown Timer
+
+Description:
+   Create a timer with the ability to set a countdown time.
+
+*/
+
+// Your code for Task 13 goes here...
+
+//=========================================================
