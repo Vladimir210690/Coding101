0a065b5e6fb1b380b1161ec69b9b9b529e6b354d
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:52:28 +0300

    Create task2_rock_paper_scissors.cpp

---
+++ task2_rock_paper_scissors.cpp
@@ -0,0 +1,12 @@
+/*
+Task 2: Console Game "Rock, Paper, Scissors"
+
+Description:
+   Implement a version of the "Rock, Paper, Scissors" game
+   between the user and the computer.
+
+*/
+
+// Your code for Task 2 goes here...
+
+//=========================================================
