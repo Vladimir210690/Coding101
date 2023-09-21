96c97d5d6801ad84e4cbce7fec0ca63863472415
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:57:20 +0300

    Create task10_discount_code_generator.cpp

---
+++ task10_discount_code_generator.cpp
@@ -0,0 +1,11 @@
+/*
+Task 10: Discount Code and Promotion Generator
+
+Description:
+   Create a tool for generating unique discount and promotion codes.
+
+*/
+
+// Your code for Task 10 goes here...
+
+//=========================================================
