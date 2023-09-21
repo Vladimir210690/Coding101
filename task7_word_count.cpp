d1af29968433e17b98bcfcae44d6ee9d341261b5
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:55:37 +0300

    Create task7_word_count.cpp

---
+++ task7_word_count.cpp
@@ -0,0 +1,11 @@
+/*
+Task 7: Word Count in Text
+
+Description:
+   Develop a program to count words in a text.
+
+*/
+
+// Your code for Task 7 goes here...
+
+//=========================================================
