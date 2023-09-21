631d8e38cb3b3ab81fee880079b89fd26df45e78
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:56:07 +0300

    Create task8_note_organizer.cpp

---
+++ task8_note_organizer.cpp
@@ -0,0 +1,11 @@
+/*
+Task 8: Note Organizer
+
+Description:
+   Create an application for creating and managing notes and tasks.
+
+*/
+
+// Your code for Task 8 goes here...
+
+//=========================================================
