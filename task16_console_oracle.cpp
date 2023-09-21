035cb93681525691489a5209b4bba071de6ecd13
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 23:00:36 +0300

    Create task16_console_oracle.cpp

---
+++ task16_console_oracle.cpp
@@ -0,0 +1,11 @@
+/*
+Task 16: Console Oracle with Predictions
+
+Description:
+   Build an application that answers user questions with random predictions.
+
+*/
+
+// Your code for Task 16 goes here...
+
+//=========================================================
