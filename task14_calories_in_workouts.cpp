492ffe76ae9da267004c93310ad999f1ce716c2d
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:59:24 +0300

    Create task14_calories_in_workouts.cpp

---
+++ task14_calories_in_workouts.cpp
@@ -0,0 +1,11 @@
+/*
+Task 14: Calories Burned in Workouts
+
+Description:
+   Develop an application for calculating burned calories in workouts with the option to enter exercise type and duration.
+
+*/
+
+// Your code for Task 14 goes here...
+
+//=========================================================
