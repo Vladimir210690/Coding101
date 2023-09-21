b6893906d2adb113daaaec2dd73a1c4a608225c3
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:53:08 +0300

    Create task3_typing_speed_tester.cpp

---
+++ task3_typing_speed_tester.cpp
@@ -0,0 +1,12 @@
+/*
+Task 3: Typing Speed Tester
+
+Description:
+   Create an application that provides text for typing
+   and measures typing speed.
+
+*/
+
+// Your code for Task 3 goes here...
+
+//=========================================================
