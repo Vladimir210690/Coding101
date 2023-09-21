33d81d8435b22ab488f7113d5463b111585760d2
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 23:03:25 +0300

    Create task20_wifi_password_generator.cpp

---
+++ task20_wifi_password_generator.cpp
@@ -0,0 +1,11 @@
+/*
+Task 20: Wi-Fi Network Password Generator
+
+Description:
+   Develop a program for generating secure passwords for Wi-Fi networks.
+
+*/
+
+// Your code for Task 20 goes here...
+
+//=========================================================
