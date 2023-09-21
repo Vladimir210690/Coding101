ae9c1b001e7f3b1a818f28270829d634d09258ac
Alex Gorin <128217174+gorinator@users.noreply.github.com>
2023-09-21 22:55:06 +0300

    Create task6_joke_generator.cpp

---
+++ task6_joke_generator.cpp
@@ -0,0 +1,11 @@
+/*
+Task 6: Joke Generator
+
+Description:
+   Create a program that generates random jokes and humor.
+
+*/
+
+// Your code for Task 6 goes here...
+
+//=========================================================
