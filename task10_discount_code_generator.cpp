ceaebc2ab6a3622d467a78bef7d8afd46a7f6cdd
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-11-23 20:39:30 +0300

    Update task10_discount_code_generator.cpp

--- task10_discount_code_generator.cpp
+++ task10_discount_code_generator.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
 	
 	int col = 1; // colichestvo simvolov v code
-	int code = 0; // peremennaia dlia generacii coda
-
+	char code = 0; // peremennaia dlia generacii coda
+	
 	int col2 = 1;
 	while (col != 0) {
 		system("cls");
@@ -15,10 +15,14 @@ int main() {
 		cout << "Input colichestvo cifr dlia coda: " << endl;
 		cin >> col;
 		
-		for (int i = 0; i < col; i++) {
-			code = rand() % 10;
-			cout << "Vash code: " << code << endl;
+		for (int i = 0; i < col;) {
+			code = rand() % 90;
+			if (code > 65) {
+				cout << "Vash code: " << code << endl;
+				i++;
+			}
 		}
+		
 		system("pause");
 	}
 	return 0;
