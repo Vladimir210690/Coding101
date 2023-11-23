4b33975b4f938e950b16f5a62eeee69368f7264d
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-11-23 19:42:05 +0300

    Update task10_discount_code_generator.cpp

--- task10_discount_code_generator.cpp
+++ task10_discount_code_generator.cpp
@@ -1,7 +1,26 @@
-/*
-Задача 10: Генератор кодов для скидок и акций
+//Program dlia generacii codov
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+	
+	int col = 1; // colichestvo simvolov v code
+	int code = 0; // peremennaia dlia generacii coda
+	int col2 = 0;
+	while (col != 0) {
+		system("cls");
+		srand(time(NULL));
 
-Описание:
-   Создать инструмент для генерации уникальных кодов для скидок и акций.
-
-*/
+		cout << "Input colichestvo cifr dlia coda: " << endl;
+		cin >> col;
+		col2 = col;
+		for (int i = 0; i < col; i++) {
+			col2 *= 10;
+		}
+		col2 /= col;
+		code = rand() % col2;
+		cout << "Vash code: " << code << endl;
+		system("pause");
+	}
+	return 0;
+}
